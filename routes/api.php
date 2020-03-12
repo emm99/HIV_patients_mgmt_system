59010c6a9bd3c7fed74f1faf1acaef90bbc4f5d1
unknown <ngungulu.emmanuel@gmail.com>
2020-03-13 02:54:23 +0300

    AUTH API

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+Route::group([
+
+
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
